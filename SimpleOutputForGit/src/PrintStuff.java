3306c7a7a0d75c34c3cfc687dfcfaf4a6b9e05e9
Bergin Dedej <bergin.dedej@rebirthofmartialarts.com>
2015-05-26 23:31:30 -0400

    this time with pushing

--- SimpleOutputForGit/src/PrintStuff.java
+++ SimpleOutputForGit/src/PrintStuff.java
@@ -3,5 +3,6 @@ public class PrintStuff {
 	public PrintStuff(){
 		System.out.println("yo i printed stuff");
 		System.out.println("is this shit for real");
+		System.out.println("lets try pushing too");
 	}
 }
